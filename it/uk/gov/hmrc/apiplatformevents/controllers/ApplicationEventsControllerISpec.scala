b40609906eb75ede8c8b85e14f1035c068623d5b
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2020-10-19 15:01:00 +0100

    API-4609b: added mongo check to integration tests

--- it/uk/gov/hmrc/apiplatformevents/controllers/ApplicationEventsControllerISpec.scala
+++ it/uk/gov/hmrc/apiplatformevents/controllers/ApplicationEventsControllerISpec.scala
@@ -5,55 +5,98 @@ import org.scalatestplus.play.ServerProvider
 import play.api.libs.ws.{WSClient, WSResponse}
 import uk.gov.hmrc.apiplatformevents.support.{AuditService, ServerBaseISpec}
 
-class ApplicationEventsControllerISpec extends ServerBaseISpec  with AuditService with  BeforeAndAfterEach {
+import uk.gov.hmrc.mongo.MongoSpecSupport
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.global
+
+import uk.gov.hmrc.apiplatformevents.repository.ApplicationEventsRepository
+import uk.gov.hmrc.apiplatformevents.models.TeamMemberAddedEvent
+import java.{util => ju}
+import uk.gov.hmrc.apiplatformevents.models.common.ApplicationEvent
+import uk.gov.hmrc.apiplatformevents.models.TeamMemberRemovedEvent
+import uk.gov.hmrc.apiplatformevents.models.ClientSecretAddedEvent
+import uk.gov.hmrc.apiplatformevents.models.ClientSecretRemovedEvent
+import uk.gov.hmrc.apiplatformevents.models.RedirectUrisUpdatedEvent
+import uk.gov.hmrc.apiplatformevents.models.ApiSubscribedEvent
+import uk.gov.hmrc.apiplatformevents.models.ApiUnsubscribedEvent
+import uk.gov.hmrc.apiplatformevents.models.PpnsCallBackUriUpdatedEvent
+
+
+class ApplicationEventsControllerISpec extends ServerBaseISpec with MongoSpecSupport with AuditService with  BeforeAndAfterEach {
 
   this: Suite with ServerProvider =>
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+      primeAuditService()
+    dropMongoDb()(global)
+  }
+
+  def dropMongoDb()(implicit ec: ExecutionContext): Unit = {
+   await(mongo().drop())
+}
+
+
+  def repo: ApplicationEventsRepository =
+    app.injector.instanceOf[ApplicationEventsRepository]
+
 
   val url = s"http://localhost:$port/application-events"
 
   val wsClient: WSClient = app.injector.instanceOf[WSClient]
 
-  override def beforeEach(): Unit = {
-   super.beforeEach()
-    primeAuditService()
-  }
+  val applicationId = ju.UUID.randomUUID.toString
+  val teamMemberEmail =  "bob@bob.com"
+  val adminRole = "ADMIN"
+  val actorId = "123454654"
+  val actorTypeGK = "GATEKEEPER"
+  val eventDateTimeString = "2014-01-01T13:13:34.441Z"
+  val clientSecretId = "abababab"
+  val oldRedirectUri = "oldrdu"
+  val newRedirectUri = "newrdu"
+  val apiContext = "apicontext"
+  val apiVersion = "1.0"
+  val boxId = "someBoxId"
+  val oldCallbackUrl = "oldUrl"
+  val newCallbackUrl = "newUrl"
 
   val validTeamMemberJsonBody: String =
-    raw"""{"applicationId": "akjhjkhjshjkhksaih",
-         |"eventDateTime": "2014-01-01T13:13:34.441Z",
-         |"actor": { "id": "123454654", "actorType": "GATEKEEPER" },
-         |"teamMemberEmail": "bob@bob.com",
-         |"teamMemberRole": "ADMIN"}""".stripMargin
+    raw"""{"applicationId": "$applicationId",
+         |"eventDateTime": "$eventDateTimeString",
+         |"actor": { "id": "$actorId", "actorType": "$actorTypeGK" },
+         |"teamMemberEmail": "$teamMemberEmail",
+         |"teamMemberRole": "$adminRole"}""".stripMargin
 
   val validClientSecretJsonBody: String =
-    raw"""{"applicationId": "akjhjkhjshjkhksaih",
-         |"eventDateTime": "2014-01-01T13:13:34.441Z",
-         |"actor": { "id": "123454654", "actorType": "GATEKEEPER" },
-         |"clientSecretId": "abababab"}""".stripMargin
+  raw"""{"applicationId": "$applicationId",
+        |"eventDateTime": "$eventDateTimeString",
+        |"actor": { "id": "$actorId", "actorType": "$actorTypeGK" },
+        |"clientSecretId": "$clientSecretId"}""".stripMargin
 
   val validRedirectUrisUpdatedJsonBody: String =
-    raw"""{"applicationId": "akjhjkhjshjkhksaih",
-         |"eventDateTime": "2014-01-01T13:13:34.441Z",
-         |"actor": { "id": "123454654", "actorType": "GATEKEEPER" },
-         |"oldRedirectUris": "oldrdu",
-         |"newRedirectUris": "newrdu"}""".stripMargin
+    raw"""{"applicationId": "$applicationId",
+           |"eventDateTime": "$eventDateTimeString",
+           |"actor": { "id": "$actorId", "actorType": "$actorTypeGK" },
+           |"oldRedirectUris": "$oldRedirectUri",
+           |"newRedirectUris": "$newRedirectUri"}""".stripMargin
 
   val validApiSubscriptionJsonBody: String =
-    raw"""{"applicationId": "akjhjkhjshjkhksaih",
-         |"eventDateTime": "2014-01-01T13:13:34.441Z",
-         |"actor": { "id": "123454654", "actorType": "GATEKEEPER" },
-         |"context": "apicontext",
-         |"version": "1.0"}""".stripMargin
+    raw"""{"applicationId": "$applicationId",
+         |"eventDateTime": "$eventDateTimeString",
+         |"actor": { "id": "$actorId", "actorType": "$actorTypeGK" },
+         |"context": "$apiContext",
+         |"version": "$apiVersion"}""".stripMargin
 
   val validPpnsCallBackUpdatedJsonBody: String =
-    raw"""{"applicationId": "akjhjkhjshjkhksaih",
-         |"eventDateTime": "2014-01-01T13:13:34.441Z",
-         |"actor": { "id": "123454654", "actorType": "GATEKEEPER" },
-         |"boxId": "someBoxId",
-         |"context": "apicontext",
-         |"version": "1.0",
-         |"oldCallbackUrl": "oldUrl",
-         |"newCallbackUrl": "newUrl"}""".stripMargin
+    raw"""{"applicationId": "$applicationId",
+         |"eventDateTime": "$eventDateTimeString",
+         |"actor": { "id": "$actorId", "actorType": "$actorTypeGK" },
+         |"boxId": "$boxId",
+         |"context": "$apiContext",
+         |"version": "$apiVersion",
+         |"oldCallbackUrl": "$oldCallbackUrl",
+         |"newCallbackUrl": "$newCallbackUrl"}""".stripMargin
 
   def doGet(path: String): WSResponse = {
     wsClient
@@ -70,11 +113,25 @@ class ApplicationEventsControllerISpec extends ServerBaseISpec  with AuditServic
       .futureValue
   }
 
+ def checkCommonEventValues[A <: ApplicationEvent](event: A){
+            event.applicationId shouldBe applicationId
+            event.eventDateTime.toString() shouldBe eventDateTimeString
+            event.actor.id shouldBe actorId
+            event.actor.actorType.toString shouldBe actorTypeGK
+ }
+
   "ApplicationEventsController" when {
 
     "POST /teamMemberAdded" should {
       "respond with 201 when valid json is sent" in {
         testSuccessScenario("/teamMemberAdded", validTeamMemberJsonBody)
+        val results = await(repo.findAll()(global))
+        results.size shouldBe 1
+        val event = results.head.asInstanceOf[TeamMemberAddedEvent]
+
+        checkCommonEventValues(event)
+        event.teamMemberEmail shouldBe teamMemberEmail
+        event.teamMemberRole shouldBe adminRole
       }
 
       "handle error scenarios correctly" in {
@@ -85,6 +142,14 @@ class ApplicationEventsControllerISpec extends ServerBaseISpec  with AuditServic
     "POST /teamMemberRemoved" should {
       "respond with 201 when valid json is sent" in {
          testSuccessScenario("/teamMemberRemoved", validTeamMemberJsonBody)
+
+        val results = await(repo.findAll()(global))
+        results.size shouldBe 1
+        val event = results.head.asInstanceOf[TeamMemberRemovedEvent]
+
+        checkCommonEventValues(event)
+        event.teamMemberEmail shouldBe teamMemberEmail
+        event.teamMemberRole shouldBe adminRole
       }
 
      "handle error scenarios correctly" in {
@@ -95,6 +160,15 @@ class ApplicationEventsControllerISpec extends ServerBaseISpec  with AuditServic
     "POST /clientSecretAdded" should {
       "respond with 201 when valid json is sent" in {
          testSuccessScenario("/clientSecretAdded", validClientSecretJsonBody)
+
+
+        val results = await(repo.findAll()(global))
+        results.size shouldBe 1
+        val event = results.head.asInstanceOf[ClientSecretAddedEvent]
+
+        checkCommonEventValues(event)
+        event.clientSecretId shouldBe clientSecretId
+        
       }
 
      "handle error scenarios correctly" in {
@@ -105,6 +179,13 @@ class ApplicationEventsControllerISpec extends ServerBaseISpec  with AuditServic
     "POST /clientSecretRemoved" should {
       "respond with 201 when valid json is sent" in {
         testSuccessScenario("/clientSecretRemoved", validClientSecretJsonBody)
+
+        val results = await(repo.findAll()(global))
+        results.size shouldBe 1
+        val event = results.head.asInstanceOf[ClientSecretRemovedEvent]
+
+        checkCommonEventValues(event)
+        event.clientSecretId shouldBe clientSecretId
       }
 
       "handle error scenarios correctly" in {
@@ -115,6 +196,14 @@ class ApplicationEventsControllerISpec extends ServerBaseISpec  with AuditServic
     "POST /redirectUrisUpdated" should {
       "respond with 201 when valid json is sent" in {
         testSuccessScenario("/redirectUrisUpdated", validRedirectUrisUpdatedJsonBody)
+
+        val results = await(repo.findAll()(global))
+        results.size shouldBe 1
+        val event = results.head.asInstanceOf[RedirectUrisUpdatedEvent]
+
+        checkCommonEventValues(event)
+        event.oldRedirectUris shouldBe oldRedirectUri
+        event.newRedirectUris shouldBe newRedirectUri
       }
 
       "handle error scenarios correctly" in {
@@ -125,6 +214,13 @@ class ApplicationEventsControllerISpec extends ServerBaseISpec  with AuditServic
     "POST /apiSubscribed" should {
       "respond with 201 when valid json is sent" in {
         testSuccessScenario("/apiSubscribed", validApiSubscriptionJsonBody)
+        val results = await(repo.findAll()(global))
+        results.size shouldBe 1
+        val event = results.head.asInstanceOf[ApiSubscribedEvent]
+
+        checkCommonEventValues(event)
+        event.context shouldBe apiContext
+        event.version shouldBe apiVersion
       }
 
      "handle error scenarios correctly" in {
@@ -135,6 +231,13 @@ class ApplicationEventsControllerISpec extends ServerBaseISpec  with AuditServic
     "POST /apiUnsubscribed" should {
       "respond with 201 when valid json is sent" in {
         testSuccessScenario("/apiUnsubscribed", validApiSubscriptionJsonBody)
+       val results = await(repo.findAll()(global))
+        results.size shouldBe 1
+        val event = results.head.asInstanceOf[ApiUnsubscribedEvent]
+
+        checkCommonEventValues(event)
+        event.context shouldBe apiContext
+        event.version shouldBe apiVersion
       }
 
      "handle error scenarios correctly" in {
@@ -145,6 +248,17 @@ class ApplicationEventsControllerISpec extends ServerBaseISpec  with AuditServic
     "POST /ppnsCallbackUriUpdated" should {
       "respond with 201 when valid json is sent" in {
         testSuccessScenario("/ppnsCallbackUriUpdated", validPpnsCallBackUpdatedJsonBody)
+
+          val results = await(repo.findAll()(global))
+        results.size shouldBe 1
+        val event = results.head.asInstanceOf[PpnsCallBackUriUpdatedEvent]
+
+        checkCommonEventValues(event)
+        event.boxId shouldBe boxId
+        event.oldCallbackUrl shouldBe oldCallbackUrl
+        event.newCallbackUrl shouldBe newCallbackUrl
+        event.context shouldBe apiContext
+        event.version shouldBe apiVersion
       }
 
       "handle error scenarios correctly" in {
--- it/uk/gov/hmrc/apiplatformevents/support/ServerBaseISpec.scala
+++ it/uk/gov/hmrc/apiplatformevents/support/ServerBaseISpec.scala
@@ -4,11 +4,18 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Seconds, Span}
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
 
 abstract class ServerBaseISpec
   extends BaseISpec with GuiceOneServerPerSuite with TestApplication with ScalaFutures {
 
   override implicit lazy val app: Application = appBuilder.build()
+
+   override protected def appBuilder: GuiceApplicationBuilder =
+    new GuiceApplicationBuilder()
+      .configure(
+        "mongodb.uri" -> s"mongodb://127.0.0.1:27017/test-${this.getClass.getSimpleName}"
+      )
 
   implicit override val patienceConfig: PatienceConfig =
     PatienceConfig(timeout = Span(4, Seconds), interval = Span(1, Seconds))
