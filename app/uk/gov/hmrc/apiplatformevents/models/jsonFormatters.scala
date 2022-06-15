025eb793c878b00e9b30468e1b0ff43bba469f11
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2022-06-15 12:36:14 +0100

    API-5376: fix datetime persistence

--- app/uk/gov/hmrc/apiplatformevents/models/jsonFormatters.scala
+++ app/uk/gov/hmrc/apiplatformevents/models/jsonFormatters.scala
@@ -19,9 +19,14 @@ package uk.gov.hmrc.apiplatformevents.models
 
 import play.api.libs.json._
 import uk.gov.hmrc.apiplatformevents.models.common.{Actor, EventId, EventType}
+import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats
 import uk.gov.hmrc.play.json.Union
 
+import java.time.LocalDateTime
+
 object MongoFormatters {
+
+  implicit val formatLocalDateTimes: Format[LocalDateTime] = MongoJavatimeFormats.localDateTimeFormat
 
   implicit val eventIdFormat: Format[EventId] = Json.valueFormat[EventId]
   implicit val actorFormat: OFormat[Actor] = Json.format[Actor]
@@ -45,7 +50,7 @@ object MongoFormatters {
     .and[ApiUnsubscribedEvent](EventType.API_UNSUBSCRIBED.toString)
     .format
 
-  val mongoCodecs = 
+  val mongoCodecs =
       Codecs.unionCodecs[ApplicationEvent](formatApplicationEvent)
 
   implicit val formatNotification: OFormat[Notification] = Json.format[Notification]
@@ -69,7 +74,7 @@ object JsonRequestFormatters {
     .and[ClientSecretAddedEvent](EventType.CLIENT_SECRET_ADDED.toString)
     .and[ClientSecretRemovedEvent](EventType.CLIENT_SECRET_REMOVED.toString)
     .and[RedirectUrisUpdatedEvent](EventType.REDIRECT_URIS_UPDATED.toString)
-    .and[PpnsCallBackUriUpdatedEvent](EventType.PPNS_CALLBACK_URI_UPDATED.toString())
+    .and[PpnsCallBackUriUpdatedEvent](EventType.PPNS_CALLBACK_URI_UPDATED.toString)
     .and[ApiSubscribedEvent](EventType.API_SUBSCRIBED.toString)
     .and[ApiUnsubscribedEvent](EventType.API_UNSUBSCRIBED.toString)
     .format
