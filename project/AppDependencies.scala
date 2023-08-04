4b6fc69a49be28478e87944caf9231c22646c03b
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-08-04 10:55:44 +0100

    API-7278 - Bump event lib to keep up to date - no impact

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,7 +15,7 @@ object AppDependencies {
     "com.enragedginger"       %% "akka-quartz-scheduler"            % "1.9.1-akka-2.6.x",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"               % hmrcMongoVersion,
     "org.typelevel"           %% "cats-core"                        % "2.9.0",
-    "uk.gov.hmrc"             %% "api-platform-application-events"  % "0.22.0"
+    "uk.gov.hmrc"             %% "api-platform-application-events"  % "0.23.0"
   )
 
   private lazy val testDependencies = Seq(
