a19b8bcc274a02112f9091d34e42a66e34049fef
John Green <john.green@digital.hmrc.gov.uk>
2024-10-16 09:25:37 +0100

    API-7905 Bump app-events library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,8 @@ object AppDependencies {
 
   lazy val bootstrapVersion    = "9.0.0"
   lazy val hmrcMongoVersion    = "1.7.0"
-  lazy val commonDomainVersion = "0.13.0"
+  lazy val commonDomainVersion = "0.17.0"
+  lazy val appEventsVersion    = "0.66.4"
 
   private lazy val dependencies = Seq(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"       % bootstrapVersion,
@@ -13,15 +14,15 @@ object AppDependencies {
     "com.github.blemale"     %% "scaffeine"                       % "5.2.1",
     "io.github.samueleresca" %% "pekko-quartz-scheduler"          % "1.1.0-pekko-1.0.x",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"              % hmrcMongoVersion,
-    "uk.gov.hmrc"            %% "api-platform-application-events" % "0.63.0"
+    "uk.gov.hmrc"            %% "api-platform-application-events" % appEventsVersion
   )
 
   private lazy val testDependencies = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"          % bootstrapVersion,
-    "org.mockito"            %% "mockito-scala-scalatest"         % "1.17.29",
-    "org.scalatest"          %% "scalatest"                       % "3.2.17",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"         % hmrcMongoVersion,
-    "com.github.tomakehurst"  % "wiremock-jre8-standalone"        % "2.35.0",
-    "uk.gov.hmrc"            %% "api-platform-test-common-domain" % commonDomainVersion
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30"              % bootstrapVersion,
+    "org.mockito"            %% "mockito-scala-scalatest"             % "1.17.29",
+    "org.scalatest"          %% "scalatest"                           % "3.2.17",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"             % hmrcMongoVersion,
+    "com.github.tomakehurst"  % "wiremock-jre8-standalone"            % "2.35.0",
+    "uk.gov.hmrc"            %% "api-platform-common-domain-fixtures" % commonDomainVersion
   ).map(m => m % "test")
 }
