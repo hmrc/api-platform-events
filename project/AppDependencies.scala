1a524b1dfd9b9a5cfca7a97d98e8c418771b1f8b
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2024-12-03 08:27:10 +0000

    API-8024 - Bump libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ object AppDependencies {
 
   lazy val bootstrapVersion    = "9.0.0"
   lazy val hmrcMongoVersion    = "1.7.0"
-  lazy val commonDomainVersion = "0.17.0"
-  lazy val appEventsVersion    = "0.67.0"
+  lazy val commonDomainVersion = "0.18.0"
+  lazy val appEventsVersion    = "0.68.0"
 
   private lazy val dependencies = Seq(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"       % bootstrapVersion,
