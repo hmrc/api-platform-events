d4fe5e1a4a39c4b5f3620366ac0f9d115562193f
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2025-01-07 14:54:54 +0000

    API-8069: bump app events library with non tolerant parsing of old application model

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,11 @@ object AppDependencies {
 
   lazy val bootstrapVersion    = "9.0.0"
   lazy val hmrcMongoVersion    = "1.7.0"
-  lazy val commonDomainVersion = "0.18.0"
   lazy val appEventsVersion    = "0.70.0"
   lazy val appDomainVersion    = "0.67.0"
 
   private lazy val dependencies = Seq(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"       % bootstrapVersion,
-    "uk.gov.hmrc"            %% "domain-play-30"                  % "9.0.0",
-    "com.github.blemale"     %% "scaffeine"                       % "5.2.1",
     "io.github.samueleresca" %% "pekko-quartz-scheduler"          % "1.1.0-pekko-1.0.x",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"              % hmrcMongoVersion,
     "uk.gov.hmrc"            %% "api-platform-application-events" % appEventsVersion
@@ -25,7 +22,6 @@ object AppDependencies {
     "org.scalatest"          %% "scalatest"                           % "3.2.17",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"             % hmrcMongoVersion,
     "com.github.tomakehurst"  % "wiremock-jre8-standalone"            % "2.35.0",
-    "uk.gov.hmrc"            %% "api-platform-common-domain-fixtures" % commonDomainVersion,
     "uk.gov.hmrc"            %% "api-platform-application-domain-fixtures" % appDomainVersion
   ).map(m => m % "test")
 }
