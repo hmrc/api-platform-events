91d186c8281948f14fb134914aacf89666f84060
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-11-05 15:51:23 +0000

    API-8734 - Migrate to OQE (#111)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ object AppDependencies {
 
   lazy val bootstrapVersion    = "9.19.0"
   lazy val hmrcMongoVersion    = "2.7.0"
-  lazy val appEventsVersion    = "0.88.0" // Ensure this version of the application-events library uses the appDomainVersion below
-  lazy val appDomainVersion    = "0.87.0"
+  lazy val appEventsVersion    = "0.90.0" // Ensure this version of the application-events library uses the appDomainVersion below
+  lazy val appDomainVersion    = "0.90.0"
 
   private lazy val dependencies = Seq(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30"       % bootstrapVersion,
@@ -18,8 +18,7 @@ object AppDependencies {
 
   private lazy val testDependencies = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"              % bootstrapVersion,
-    "org.mockito"            %% "mockito-scala-scalatest"             % "1.17.29",
-    "org.scalatest"          %% "scalatest"                           % "3.2.17",
+    "org.mockito"            %% "mockito-scala-scalatest"             % "2.0.0",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"             % hmrcMongoVersion,
     "com.github.tomakehurst"  % "wiremock-jre8-standalone"            % "2.35.0",
     "uk.gov.hmrc"            %% "api-platform-application-domain-fixtures" % appDomainVersion
